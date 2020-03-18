aedd4217e0bf20e3fbd206f93bcdb9cbd6250e7b
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2020-03-18 18:21:10 +0900

    Update 5mins.py

--- 5 Minutes Morse Code/5mins.py
+++ 5 Minutes Morse Code/5mins.py
@@ -2,6 +2,7 @@
 
 import random
 import os
+import re
 
 MORSE_CODE_CHART = {'A':'.-',
                     'B':'-...',
@@ -66,8 +67,25 @@ def encrypt(message):
             cipher += ' '
     return cipher
 
+def decrypt(message):
+    message += ' '
+    decipher = ''
+    citext = ''
+    for letter in message:
+        if letter != ' ':
+            i = 0
+            citext += letter
+        else:
+            i += 1
+            if i == 2:
+                decipher += ' '
+            else:
+                decipher += list(MORSE_CODE_CHART.keys())[list(MORSE_CODE_CHART.values()).index(citext)]
+                citext = ''
+    return decipher
 
-def main():
+
+def morse_word():
     total = 0
     points = 0
     while True:
@@ -77,15 +95,57 @@ def main():
         word_morse = encrypt(words.upper().rstrip())
         
         print("Word: {a}".format(a=word_morse))
-        word_answer = input("Answer: ")
+        word_answer = input("> ")
         word_answer = encrypt(word_answer.upper())
         
         if word_answer == word_morse:
             print("Correct!")
             points += 1
+        elif word_answer == '':
+            main()
         else:
-            print("Wrong!\nCorrect word is \"{w}\"".format(w=words.rstrip()))
+            print("Wrong!\nCorrect word is \" {w} \"".format(w=words.rstrip()))
 
+def word_morse():
+    total = 0
+    points = 0
+    while True:
+        print("\nScore: {p}/{t}".format(p=points,t=total))
+        total += 1
+        words = get_word()
+        
+        
+        print(words)
+        word_answer = input("> ")
+        if len(word_answer) > 0:
+            
+            
+            word_answer = decrypt(word_answer)
+        else:
+            main()        
+
+        words = words.rstrip().upper()
+        if word_answer == words:
+            print("Correct!")
+            points += 1
+        elif word_answer == '':
+            main()
+        else:
+            print("Wrong!\nCorrect code is \" {w} \"".format(w=encrypt(words)))
+
+def main():
+
+    os.system('cls')
+    print('''1) Convert Morse Code into English Text
+2) Convert English Text to Morse Code
+''')
+    rx = int(input("> "))
+    if rx == 1:
+        morse_word()
+    elif rx == 2:
+        word_morse()
+    else:
+        exit()
 
 if __name__ == "__main__":
     os.system('cls')
