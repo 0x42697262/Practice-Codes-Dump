d4567f980d6f1ba34cff96b0c4c71d3d6a16fe0f
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2020-04-20 14:15:50 +0900

    Update mine.php

--- MiningPH Bot/Web/mine.php
+++ MiningPH Bot/Web/mine.php
@@ -2,8 +2,10 @@
 $password = "Chen Zhen";
 $sentKey = "";
 $user_id = "1352408";
-$numReqs = "100";
+$numReqs = "10000";
 $URLm = "";
+
+
 
 if(isset($_POST['submit'])){
     $sentKey = htmlspecialchars($_POST['key'], ENT_QUOTES, 'UTF-8');
@@ -11,38 +13,31 @@ if(isset($_POST['submit'])){
     $user_id = htmlspecialchars($_POST['user_id'], ENT_QUOTES, 'UTF-8');
     $numReqs = htmlspecialchars($_POST['num_req'], ENT_QUOTES, 'UTF-8');
     
-
+   
     $URLm = "https://miningph.com/application/views/user/getHashes.php?xVal=1&id=".$user_id."&yVal=10000";
-    startMiner($user_id, $numReqs, $URLm);
+    
     if($sentKey == $password){
         startMiner($user_id, $numReqs, $URLm);
     }else{
         echo("Key error...");
-		
+		#cheatMine(10000);
     }
 }
 
 function startMiner($id, $reqs, $link){
-    echo("ID: ".$id."\nNumber of Requests: ".$reqs."\n");
+    echo("\nID: ".$id."\nNumber of Requests: ".$reqs."\n");
+    $errors = 0;
     for($i = 0; $i < $reqs; $i++){
-        $errors = 0;
-
-        $options = array(
-            'http' => array('method' => 'GET')
-            );
+        
+        $options = array('http' => array('method' => 'GET'));
         $context = stream_context_create($options);
         $result = file_get_contents($link, false, $context);
+
+        $result = file_get_contents('https://miningph.com/application/views/user/getHashes.php?xVal=1&id=1352408&yVal=10000', false, $context);
+        
         if ($result === FALSE){$errors += 1;}
 
-        var_dump($result);
-        echo("\nErrors: " . $errors);
-            
-        }
-        
     }
-    
-
-    
-   
-
+    echo("\nErrors: " . $errors);
+    } 
 ?> 
