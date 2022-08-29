a39c869d0accee68a83958e2bde5ea2843db77f3
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2022-08-29 22:46:46 +0900

    idk

--- args_test.py
+++ args_test.py
@@ -3,6 +3,7 @@ import sys
 def main():
     print(f"# of arguments: {len(sys.argv)}")
     print(f"Arguments List: {str(sys.argv)}")
+    print(f"{str(sys.argv[1])}")
 
 def show_output(): 
     pass
