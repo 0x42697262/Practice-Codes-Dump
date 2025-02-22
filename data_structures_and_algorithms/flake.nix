c1db02983a0119e615eb0e51a7017facedfc2377
0x42697262 <giogulane.wineffgulane@gmail.com>
2025-02-22 20:30:44 +0900

    DSA: setting it up with nix

---
+++ data_structures_and_algorithms/flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+            (python312.withPackages (modules: with modules; [
+              flask
+            ]))
+          ];
+
+          shellHook = ''
+            echo "Setup complete."
+            fish'';
+        };
+      }
+    );
+}
